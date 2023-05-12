3a089b9ea0f2f0300347293f1e6d2bf30e1f3007
Yaroslav Roshchupkin <yaroslav-r.01@mail.ru>
2023-05-13 02:14:47 +0300

    initial

---
+++ IDoctor.cs
@@ -0,0 +1,10 @@
+﻿namespace task5_csharp
+{
+    internal interface IDoctor
+    {
+        public string Name { get; protected set; }
+
+        public void Examine(String patientName);
+        public void Treat(String patientName);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace task5_csharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Neurosurgeon doctor1 = new Neurosurgeon("Doc Mitchell", "Burdenko", 15, DiagnosisMethod.CT, false);
+            Surgeon doctor2 = new Neurosurgeon("Gregory House", "Johns Hopkins University", 3, DiagnosisMethod.MRI, true);
+            List<IDoctor> doctors = new List<IDoctor> { doctor1, doctor2 };
+            doctors.ForEach(Console.WriteLine);
+
+            foreach (var doctor in doctors)
+            {
+                string patient = "Random Patient";
+                doctor.Examine(patient);
+                doctor.Treat(patient);
+            }
+            doctors.ForEach(Console.WriteLine);
+        }
+    }
+}
